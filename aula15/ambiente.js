4d8d40493b4c7c6eb0336e64065f6f823277ffe2
victorbaima <victor.baima@hotmail.com>
2020-07-02 23:09:57 -0300

    Aulas 12, 13, 14 e 15 incluídas!

---
+++ aula15/ambiente.js
@@ -0,0 +1,6 @@
+let num = [5, 8, 2, 9, 3]
+num.sort()  // Ordena os elementos do array em ordem crescente
+num.push(1)  // Adiciona o valor ao array em uma nova posição
+console.log(num)
+console.log(`O vetor tem ${num.length} posições`)
+console.log(`O primeiro valor do vetor é ${num[0]}`)
